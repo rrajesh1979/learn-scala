fdfb5805e0257df75ec4dd4ea394d8c465e334cf
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-07-18 07:13:06 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "learn-scala"
+
+version := "0.1"
+
+scalaVersion := "3.0.2-RC1"
---
+++ src/main/scala/ScalaBasics.scala
@@ -0,0 +1,28 @@
+import scala.language.postfixOps
+
+object ScalaBasics extends App {
+  class Person(val firstName: String, lastName: String, age: Int) {
+    val fullName: String = firstName + " " + lastName
+    def getName: String = fullName
+    def greeting: String = s"Hello Scala from $fullName"
+    def likes(movie: String): String = s"$fullName likes $movie"
+
+    def !!(progLanguage: String): String =
+      s"$fullName wonders how can $progLanguage be so cool!"
+
+    def unary_! : String = s"This is from a unary operator :: {$age}"
+
+    def apply(num: Int): String =
+      s"Apply operator :: $num :: $fullName"
+  }
+
+  val john: Person = new Person("John", "Doe", 50)
+  println(john.firstName)
+  println(john.greeting)
+
+  println(john likes "Harry Potter")
+
+  println(!john)
+
+  println(john(100))
+}
