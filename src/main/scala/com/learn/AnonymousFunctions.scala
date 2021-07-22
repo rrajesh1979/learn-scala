110ffa638e63e2f422a60e3a2edbd2ea0f785fd1
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-07-22 17:45:02 -0500

    updates to AnonymousFunctions

--- src/main/scala/com/learn/AnonymousFunctions.scala
+++ src/main/scala/com/learn/AnonymousFunctions.scala
@@ -2,21 +2,33 @@ package com.learn
 
 object AnonymousFunctions extends App {
 
-  val doubler = new Function[Int, Int] {
-    override def apply(x: Int): Int = x * 2
+  val doublerA = new Function1[Int, Int] {
+    override def apply(v1: Int): Int = v1 * 2
   }
+  println(doublerA(2))
 
-  val doublerF: (Int => Int) = (x: Int) => x * 2
+  val doublerB: (Int => Int) = (x: Int) => x * 2
+  println(doublerB(3))
 
-  val doublerFF: (Int => Int) =
-    x => x * 2
+  val doublerC = (x: Int) => x * 2
+  println(doublerC(4))
 
-  println(doubler(2))
+  val doublerD: (Int => Int) = x => x * 2
+  println(doublerC(5))
 
-  println(doublerF(5))
+  val doublerE: Int => Int = _ * 2
+  println(doublerE(5))
 
-  val adder = (a: Int, b: Int) => a + b
-  val adderF: (Int, Int) => Int = (a, b) => a + b
+  val adderA: (Int, Int) => Int = (x, y) => x + y
+  println(adderA(5, 6))
 
+  val adderB: (Int, Int) => Int = _ + _
+  println(adderB(5, 6))
+
+  val noParamA: () => Int = () => 3
+  val noParamB = () => 3
+
+  println(noParamA())
+  println(noParamB())
 
 }
--- src/main/scala/com/learn/LearnVariance.scala
+++ src/main/scala/com/learn/LearnVariance.scala
@@ -1,5 +1,14 @@
 package com.learn
 
-object LearnVariance {
+object LearnVariance extends App {
+
+  class Animal
+  class Lion extends Animal
+  class Fish extends Animal
+
+  //Covariance
+  class CVList[+A]
+
+  val cvAnimalList: CVList[Animal] = new CVList[Lion]
 
 }
